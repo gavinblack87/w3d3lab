e5e4bea7a12df93edf7fe14a93a9a2da53468f0a
Josh Gibson <flash50k@gmail.com>
2019-07-10 15:29:00 +0100

    saves done and working

--- db/console.rb
+++ db/console.rb
@@ -3,12 +3,24 @@ require_relative('../models/album.rb')
 require_relative('../models/artist.rb')
 
 Artist.delete_all()
-# Album.delete_all()
+Album.delete_all()
 
 artist1 = Artist.new({'name' => 'The Beatles'})
 artist2 = Artist.new({'name' => 'The Rolling Stones'})
 artist1.save()
 artist2.save()
+
+album1 = Album.new({'title' => 'Abbey Road',
+                      'genre' => 'Classic Rock',
+                      'artist_id' => artist1.id})
+
+album2 = Album.new({'title' => 'Let It Bleed',
+                      'genre' => 'Rock',
+                      'artist_id' => artist2.id})
+
+
+album1.save()
+album2.save()
 
 binding.pry
 nil
